21059c81e255a36e90c7063edfb070b7390bcada
OseiasDeluca <89862166+OseiasDeluca@users.noreply.github.com>
2021-11-21 21:07:01 -0300

    Add files via upload

---
+++ Aula02 - Variaveis/script.js
@@ -0,0 +1,73 @@
+//var vOUf = false;
+//console.log(typeof(vOUf));
+
+var escopoGlobal = 'Global';
+console.log(escopoGlobal);
+
+function escopoLocal(){
+    let escopoLocalInterno = 'Local'
+    console.log(escopoLocalInterno);
+}
+
+escopoLocal();
+
+
+// Atribuição
+var atribuicao = 'Ze';
+
+//Comparação
+var comparacao = '0' == 0;
+console.log(comparacao)
+
+//Comparação identica   
+var comparacaoidentica = '0' === 0;
+console.log(comparacaoidentica);
+
+//Adição
+var adicao = 1 + 1
+console.log(adicao)
+//Subtração
+var sub = 1 - 1
+console.log(sub)
+//Multiplicação 
+var multi = 2 * 2
+console.log(multi)
+//Divisão Real
+var div = 10 / 2
+console.log(div)
+
+//Resto da Divisão
+var divres = 5 % 2  
+console.log(divres)
+
+// Potenciação
+var potenciacao = 2 ** 10
+console.log(potenciacao)
+
+// Maior que
+var maiorque = 5 > 2
+console.log(maiorque)
+
+// Menor que
+var menorque = 5 < 2
+console.log(menorque)
+
+//Maior ou igual
+var maiorouigual = 5 >= 2
+console.log(maiorouigual)
+
+//Menor ou igual
+var menorouigual = 5 <= 2
+console.log(menorouigual)
+
+// And
+var e = true && true
+console.log(e)
+
+// Or
+var ou = true || false
+console.log(ou)
+
+// No
+var nao = !true
+console.log(nao)
---
+++ Aula03 - Array e Objeto/script.js
@@ -0,0 +1,43 @@
+//Vetores e Objetos     
+
+// Como declarar um array
+let array2 = ['string', 1, true];
+console.log(array2)
+
+// Pode guardar vários tipos de dados
+let array = ['string', 1, true, ['array1'], ['array2'], ['array3'], ['array4']];
+//console.log(array2[3])
+
+// Manipulando Arrays
+
+// ForEach - Itera um array;
+// array.forEach(function(item, index){console.log(item, index)})
+
+/// Push() - add item no final do array;
+/* array.push('novo item')
+console.log(array2)*/
+
+/*
+// Pop() - Remove item no final do array;
+array.pop()
+console.log(array)
+
+// Shift() - Remove um item do começo do array ;
+array.shift()
+console.log(array)
+
+// Unshift() - Add item no início do array;
+array.unshift('New item')
+console.log(array)
+
+// indexOf() - Retorna o índice de um valor;
+console.log(array.indexOf(true))
+
+// splice() - Remove ou substitui um item pelo índice;
+array.splice(0, 3)
+console.log(array) 
+
+// Slice() - Retorna uma parte de um array existente;
+let novoArray = array.slice(0, 3);
+console.log(novoArray)
+*/
